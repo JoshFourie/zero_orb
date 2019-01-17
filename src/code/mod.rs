67c2cb2888b4234e950972d3889fb12ab1d7c1e8
JoshFourie <josh@omniaprotocol.com>
2019-01-17 18:06:11 +1100

    code: added and & or logic gates.

--- src/code/mod.rs
+++ src/code/mod.rs
@@ -1,19 +1,62 @@
-pub mod comparator;
-
-// DO NOT USE COMPARATOR EXCEPT FOR TESTING, POTENTIAL VULNERABILITY W.R.T. TWO'S COMPLEMENT BEING INVESTIGATED,
-// FOR COMPARISON, ASSERT 0 < A < B AS A SUBSTITUTE TO AVOID A BEING > BUT BEING PARSED AS < BECAUSE OF TWO'S COMPLEMENT.
-
-// todo: improve testing to check that certain lines exist in the generated files.
-#[ignore]
 #[test]
-fn test_comparator_code_gen() {
-    use std::path::Path;
-    comparator::new("64 COMP", Path::new("src/tests/files/code_gen/comparator_64.zk"));
-    comparator::new("32 COMP", Path::new("src/tests/files/code_gen/comparator_32.zk"));
-    comparator::new("16 COMP", Path::new("src/tests/files/code_gen/comparator_16.zk"));
-    comparator::new("8 COMP", Path::new("src/tests/files/code_gen/comparator_8.zk"));
-    comparator::new("64 RANGE", Path::new("src/tests/files/code_gen/range_64.zk"));
-    comparator::new("32 RANGE", Path::new("src/tests/files/code_gen/range_32.zk"));
-    comparator::new("16 RANGE", Path::new("src/tests/files/code_gen/range_16.zk"));
-    comparator::new("8 RANGE", Path::new("src/tests/files/code_gen/range_8.zk"));
+fn test_AND_gate() {
+    use crate::knowledge::{Knowledge, Marker, zkProof, zkVerify};
+    use crate::common::{Common, CommonReference};
+    use crate::crypto::{EdDSA, SignatureScheme};
+    use crate::interface::{Andromeda, GoZero, MarkZero};
+    use zksnark::groth16::fr::{FrLocal, G1Local, G2Local};
+
+    // an AND gate is analytically represented as the product of two bits.
+
+    let crs: CommonReference<FrLocal, G1Local, G2Local> = CommonReference::new(
+        std::fs::read_to_string("src/code/and.zk").unwrap()
+    );
+    let weights = Knowledge::into(
+        None, 
+        None,
+        Some(vec![0, 1, 1, 1, 0, 1, 0, 1]),
+        None,
+        None,
+    );
+    // we are expecting that running 0111 and 0101 through an AND gate will get us 0101 back.
+    assert!(
+        Andromeda::into(
+            crs.clone(), 
+            weights, 
+            Some(vec![0, 1, 0, 1]),
+            EdDSA::<String>::init_key_pair()
+        ).go().verify()
+    );
+}
+
+#[test]
+fn test_OR_gate() {
+    use crate::knowledge::{Knowledge, Marker, zkProof, zkVerify};
+    use crate::common::{Common, CommonReference};
+    use crate::crypto::{EdDSA, SignatureScheme};
+    use crate::interface::{Andromeda, GoZero, MarkZero};
+    use zksnark::groth16::fr::{FrLocal, G1Local, G2Local};
+
+    // an OR gate is analytically represented as f(a, b) = a + b - ab.
+
+    let crs: CommonReference<FrLocal, G1Local, G2Local> = CommonReference::new(
+        std::fs::read_to_string("src/code/or.zk").expect("internal_test: reading CommonReference from string")
+    );
+    let weights = Knowledge::into(
+        None, 
+        None,
+        Some(vec![0, 1, 0b1001]),
+        None,
+        None,
+    );
+    // we are expecting that running 0111 and 0101 through an AND gate will get us 0101 back.
+    assert_eq!(
+        true,
+        Andromeda::into(
+            crs.clone(), 
+            weights, 
+            Some(vec![1]),
+            EdDSA::<String>::init_key_pair()
+        ).go().verify()
+    );
 }
